d5ce6afb2e121c76656cd3802446e3ee21e9d6b0
Daniel Lavric <dalavric@yahoo.com>
2017-10-16 12:50:18 +0300

    First commit

---
+++ app/design/frontend/Lavric/Luma_Upgraded/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 05.10.2017
+ * Time: 14:40
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Lavric/Luma_Upgraded',
+    __DIR__
+);
